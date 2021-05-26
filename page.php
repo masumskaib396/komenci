d4a0645e170d65569f2b9394c89007468e2f4497
Masum Sakib <masumsakib396@gmail.com>
2021-05-26 15:42:03 +0600

    chagne css

--- page.php
+++ page.php
@@ -14,16 +14,12 @@
 
 get_header();
 ?>
-
 	<div id="primary" class="<?php echo esc_attr( komenci_content_class() ); ?>">
 		<main id="main" class="site-main">
-
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
 			get_template_part( 'template-parts/content', 'page' );
-
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
